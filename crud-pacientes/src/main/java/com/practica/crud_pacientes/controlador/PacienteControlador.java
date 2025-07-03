7b99f6baceae3b9c2167f2d9ccc8455f860cfff5
Guadalupe Fernandez <guadalupe.fernandez@teamcubation.com>
2025-07-03 18:24:42 -0300

    refactor: remove try-catch del controller

--- crud-pacientes/src/main/java/com/practica/crud_pacientes/controlador/PacienteControlador.java
+++ crud-pacientes/src/main/java/com/practica/crud_pacientes/controlador/PacienteControlador.java
@@ -1,7 +1,6 @@
 package com.practica.crud_pacientes.controlador;
 
 import com.practica.crud_pacientes.dto.PacienteDto;
-import com.practica.crud_pacientes.excepciones.PacienteNoEncontradoException;
 import com.practica.crud_pacientes.servicio.IPacienteServicio;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,65 +31,41 @@ public class PacienteControlador {
     @GetMapping("/buscar-por-id/{idPaciente}")
     @ResponseStatus
     public ResponseEntity<?> getPaciente(@PathVariable Integer idPaciente) {
-        try{
-            PacienteDto pacienteEncontrado = pacienteServicio.getPacientePorId(idPaciente);
-            return new ResponseEntity<>(pacienteEncontrado, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
+        PacienteDto pacienteEncontrado = pacienteServicio.getPacientePorId(idPaciente);
+        return new ResponseEntity<>(pacienteEncontrado, HttpStatus.OK);
     }
 
     @GetMapping("/buscar-por-dni")
     @ResponseStatus
     public ResponseEntity<?> getPacientePorDni(@RequestParam String dni){
-        try{
-            PacienteDto pacienteEncontrado = pacienteServicio.getPacientePorDni(dni);
-            return new ResponseEntity<>(pacienteEncontrado, HttpStatus.OK);
-        }catch(RuntimeException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
+        PacienteDto pacienteEncontrado = pacienteServicio.getPacientePorDni(dni);
+        return new ResponseEntity<>(pacienteEncontrado, HttpStatus.OK);
     }
 
     @GetMapping("/buscar-por-nombre")
     @ResponseStatus
     public ResponseEntity<List<PacienteDto>> getPacientePorNombre(@RequestParam String nombre){
-        try{
-            List<PacienteDto> pacientesEncontrados = pacienteServicio.getPacientePorNombre(nombre);
-            return new ResponseEntity<>(pacientesEncontrados, HttpStatus.OK);
-        }catch (RuntimeException e){
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
+        List<PacienteDto> pacientesEncontrados = pacienteServicio.getPacientePorNombre(nombre);
+        return new ResponseEntity<>(pacientesEncontrados, HttpStatus.OK);
     }
 
     @PostMapping("/nuevo-paciente")
     @ResponseStatus
     public ResponseEntity<?> agregarPaciente(@RequestBody PacienteDto pacienteNuevo){
-        try{
-            PacienteDto nuevoPaciente = pacienteServicio.addPaciente(pacienteNuevo);
-            return new ResponseEntity<>(nuevoPaciente, HttpStatus.CREATED);
-        }catch (RuntimeException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        PacienteDto nuevoPaciente = pacienteServicio.addPaciente(pacienteNuevo);
+        return new ResponseEntity<>(nuevoPaciente, HttpStatus.CREATED);
     }
 
     @PutMapping("actualizar/{idPaciente}")
     @ResponseStatus
-    public ResponseEntity<?> actualizarPaciente(@PathVariable Integer idPaciente, @RequestBody PacienteDto paciente) throws PacienteNoEncontradoException {
-        try{
-            PacienteDto pacienteActualizado = pacienteServicio.updatePaciente(idPaciente, paciente);
-            return new ResponseEntity<>(pacienteActualizado, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<?> actualizarPaciente(@PathVariable Integer idPaciente, @RequestBody PacienteDto paciente) {
+        PacienteDto pacienteActualizado = pacienteServicio.updatePaciente(idPaciente, paciente);
+        return new ResponseEntity<>(pacienteActualizado, HttpStatus.OK);
     }
 
     @DeleteMapping("eliminar/{idPaciente}")
-    public ResponseEntity<?> eliminarPaciente(@PathVariable Integer idPaciente) throws PacienteNoEncontradoException {
-        try{
-            pacienteServicio.deletePaciente(idPaciente);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<?> eliminarPaciente(@PathVariable Integer idPaciente) {
+        pacienteServicio.deletePaciente(idPaciente);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
