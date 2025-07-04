c0c3ba88c5c9bb51d81bcf403745ff77ce2e7f7a
Guadalupe Fernandez <guadalupe.fernandez@teamcubation.com>
2025-07-04 13:14:59 -0300

    feature: add validaciones al dto

--- crud-pacientes/src/main/java/com/practica/crud_pacientes/dto/PacienteDto.java
+++ crud-pacientes/src/main/java/com/practica/crud_pacientes/dto/PacienteDto.java
@@ -1,5 +1,9 @@
 package com.practica.crud_pacientes.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
@@ -8,14 +12,27 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class PacienteDto {
 
+    @NotEmpty(message = "Debe ingresar un nombre.")
+    @Size(min = 3, max = 25)
     private String nombre;
+    @NotEmpty(message = "Debe ingresar un apellido.")
+    @Size(min = 3, max = 25)
     private String apellido;
+    @NotEmpty(message = "Debe ingresar un DNI.")
+    @Size(min = 8, max = 8, message = "Ingrese un DNI valido")
     private String dni;
+    @NotEmpty(message = "Debe ingresar una obra social.")
     private String obraSocial;
+    @NotEmpty(message = "Debe ingresar un email.")
+    @Email
     private String email;
+    @NotEmpty(message = "Debe ingresar un telefono.")
     private String telefono;
+    @NotEmpty(message = "Debe ingresar un domicilio.")
     private String domicilio;
+    @NotNull(message = "Debe ingresar la fecha de nacimiento.")
     private LocalDate fechaNacimiento;
+    @NotEmpty(message = "Debe ingresar el estado civil.")
     private String estadoCivil;
 
     public String getEstadoCivil() {
