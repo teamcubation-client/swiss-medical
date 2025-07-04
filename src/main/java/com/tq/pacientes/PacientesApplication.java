4626e1e8c1cecff8f2ea3ddf6458da9209e00c32
fercuche <fer2.maciel@gmail.com>
2025-07-04 17:49:13 -0300

    feature: add Swagger documentation

--- src/main/java/com/tq/pacientes/PacientesApplication.java
+++ src/main/java/com/tq/pacientes/PacientesApplication.java
@@ -1,9 +1,16 @@
 package com.tq.pacientes;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "API Pacientes",
+        version = "V1.0",
+        description = "API para gestionar pacientes")
+)
 public class PacientesApplication {
 
     public static void main(String[] args) {
