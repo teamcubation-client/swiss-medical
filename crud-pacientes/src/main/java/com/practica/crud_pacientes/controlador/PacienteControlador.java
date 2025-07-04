cc02aaf637cd65e66caa5c0b6b306a936bf1d704
Guadalupe Fernandez <guadalupe.fernandez@teamcubation.com>
2025-07-04 13:19:30 -0300

    refactor: remove anotacion ResponseStatus

--- crud-pacientes/src/main/java/com/practica/crud_pacientes/controlador/PacienteControlador.java
+++ crud-pacientes/src/main/java/com/practica/crud_pacientes/controlador/PacienteControlador.java
@@ -18,7 +18,6 @@ public class PacienteControlador {
     }
 
     @GetMapping("/pacientes")
-    @ResponseStatus
     public ResponseEntity<List<PacienteDto>> getPacientes() {
         try{
             List<PacienteDto> pacienteObtenido = pacienteServicio.getPacientes();
@@ -29,35 +28,30 @@ public class PacienteControlador {
     }
 
     @GetMapping("/buscar-por-id/{idPaciente}")
-    @ResponseStatus
     public ResponseEntity<?> getPaciente(@PathVariable Integer idPaciente) {
         PacienteDto pacienteEncontrado = pacienteServicio.getPacientePorId(idPaciente);
         return new ResponseEntity<>(pacienteEncontrado, HttpStatus.OK);
     }
 
     @GetMapping("/buscar-por-dni")
-    @ResponseStatus
     public ResponseEntity<?> getPacientePorDni(@RequestParam String dni){
         PacienteDto pacienteEncontrado = pacienteServicio.getPacientePorDni(dni);
         return new ResponseEntity<>(pacienteEncontrado, HttpStatus.OK);
     }
 
     @GetMapping("/buscar-por-nombre")
-    @ResponseStatus
     public ResponseEntity<List<PacienteDto>> getPacientePorNombre(@RequestParam String nombre){
         List<PacienteDto> pacientesEncontrados = pacienteServicio.getPacientePorNombre(nombre);
         return new ResponseEntity<>(pacientesEncontrados, HttpStatus.OK);
     }
 
     @PostMapping("/nuevo-paciente")
-    @ResponseStatus
     public ResponseEntity<?> agregarPaciente(@RequestBody PacienteDto pacienteNuevo){
         PacienteDto nuevoPaciente = pacienteServicio.addPaciente(pacienteNuevo);
         return new ResponseEntity<>(nuevoPaciente, HttpStatus.CREATED);
     }
 
     @PutMapping("actualizar/{idPaciente}")
-    @ResponseStatus
     public ResponseEntity<?> actualizarPaciente(@PathVariable Integer idPaciente, @RequestBody PacienteDto paciente) {
         PacienteDto pacienteActualizado = pacienteServicio.updatePaciente(idPaciente, paciente);
         return new ResponseEntity<>(pacienteActualizado, HttpStatus.OK);
