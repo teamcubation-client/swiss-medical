1e4e9a9e4cf1fc1324dbe7e00b22a283e61531f3
Guadalupe Fernandez <guadalupe.fernandez@teamcubation.com>
2025-07-07 13:15:20 -0300

    refactor: add campo errores en ErrorResponse

--- crud-pacientes/src/main/java/com/practica/crud_pacientes/excepciones/ErrorResponse.java
+++ crud-pacientes/src/main/java/com/practica/crud_pacientes/excepciones/ErrorResponse.java
@@ -2,6 +2,7 @@ package com.practica.crud_pacientes.excepciones;
 
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 
 public class ErrorResponse {
@@ -11,6 +12,7 @@ public class ErrorResponse {
     private String mensaje;
     private String error;
     private String path;
+    private Map<String, String> errores;
 
     public ErrorResponse(LocalDateTime fechaHora, int status, String mensaje, String error, String path) {
         this.fechaHora = fechaHora;
@@ -18,6 +20,23 @@ public class ErrorResponse {
         this.mensaje = mensaje;
         this.error = error;
         this.path = path;
+    }
+
+    public ErrorResponse(LocalDateTime fechaHora, int status, String mensaje, String error, String path, Map<String, String> errores) {
+        this.fechaHora = fechaHora;
+        this.status = status;
+        this.mensaje = mensaje;
+        this.error = error;
+        this.path = path;
+        this.errores = errores;
+    }
+
+    public Map<String, String> getErrores() {
+        return errores;
+    }
+
+    public void setErrores(Map<String, String> errores) {
+        this.errores = errores;
     }
 
     public String getPath() {
