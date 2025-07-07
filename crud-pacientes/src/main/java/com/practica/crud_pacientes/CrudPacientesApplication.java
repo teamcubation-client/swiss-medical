a6f4f50392db7fe49c73fe8a341cbf271a11d46b
Guadalupe Fernandez <guadalupe.fernandez@teamcubation.com>
2025-07-07 11:46:55 -0300

    docs: add OpenAPIDefinition a la clase principal

--- crud-pacientes/src/main/java/com/practica/crud_pacientes/CrudPacientesApplication.java
+++ crud-pacientes/src/main/java/com/practica/crud_pacientes/CrudPacientesApplication.java
@@ -1,8 +1,28 @@
 package com.practica.crud_pacientes;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+		info = @Info(
+				contact = @Contact(
+						name = "Guadalupe Fernandez",
+						email = "guadalupe.fernandez@teamcubation.com"
+				),
+				description = "OpenAPI documentation for CRUD Pacientes",
+				title = "CRUD Pacientes API",
+				version = "1.0"
+		),
+		servers = {
+				@Server(
+					description = "Local ENV",
+					url = "http://localhost:8080"
+				)
+		}
+)
 @SpringBootApplication
 public class CrudPacientesApplication {
 
