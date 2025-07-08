d4f4cf774169319e50da713215cb4fb24be5eba7
Marcos Hwang <marcos.hwang@teamcubation.com>
2025-07-08 12:09:19 -0300

    correccion de buenas costumbres de manejo de errores con una constante

---
+++ src/main/java/swissmedical/controller/ApiResponseConst.java
@@ -0,0 +1,23 @@
+package swissmedical.controller;
+
+public class ApiResponseConst {
+    public static final String CODE_OK = "200";
+    public static final String CODE_CREATED = "201";
+    public static final String CODE_NO_CONTENT = "204";
+    public static final String CODE_NOT_FOUND = "404";
+    public static final String CODE_INTERNAL_SERVER_ERROR = "500";
+
+    public static final String DESC_OK = "Paciente encontrado exitosamente";
+    public static final String DESC_CREATED = "Paciente creado exitosamente";
+    public static final String DESC_LIST = "Paciente listado exitosamente";
+    public static final String DESC_UPDATED = "Paciente actualizado exitosamente";
+    public static final String DESC_DELETED = "Paciente eliminado exitosamente";
+    public static final String DESC_ACTIVATED = "Paciente activado exitosamente";
+    public static final String DESC_DEACTIVATED = "Paciente Desactivado exitosamente";
+    public static final String DESC_NOT_FOUND_ID = "No existe el paciente con el ID";
+    public static final String DESC_NOT_FOUND_DNI = "No existe el paciente con el DNI";
+    public static final String DESC_NOT_FOUND_NAME = "No existe el paciente con el nombre";
+    public static final String DESC_INTERNAL_ERROR = "Error del sistema";
+    public static final String DESC_LIST_ACTIVATED = "Lista de pacientes activos";
+    public static final String DESC_LIST_DEACTIVATED = "Lista de pacientes inactivos";
+}
--- src/main/java/swissmedical/controller/IPacienteController.java
+++ src/main/java/swissmedical/controller/IPacienteController.java
@@ -7,12 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import swissmedical.dto.PacienteDTO;
 import java.util.List;
+import static swissmedical.controller.ApiResponseConst.*;
+
 
 public interface IPacienteController {
     @Operation(summary = "Listar pacientes", description = "Lista todos los pacientes en el sistema", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Pacientes listados exitosamente"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_OK, description = DESC_LIST),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description = DESC_INTERNAL_ERROR)
     })
     @GetMapping
     ResponseEntity<List<PacienteDTO>> listarPacientes();
@@ -20,8 +22,8 @@ public interface IPacienteController {
 
     @Operation(summary = "Crear un nuevo paciente", description = "Registra un nuevo paciente en el sistema", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "Paciente creado exitosamente"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_CREATED, description = DESC_CREATED),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description = DESC_INTERNAL_ERROR)
     })
     @PostMapping
     ResponseEntity<PacienteDTO> crearPaciente(@RequestBody PacienteDTO pacienteDTO);
@@ -29,9 +31,9 @@ public interface IPacienteController {
 
     @Operation(summary = "Buscar paciente por ID", description = "Busca al paciente con por el campo ID correspondiente", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Paciente encontrado exitosamente"),
-        @ApiResponse(responseCode = "404", description = "No existe el paciente con el ID"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_OK, description = DESC_OK),
+        @ApiResponse(responseCode = CODE_NOT_FOUND, description = DESC_NOT_FOUND_ID),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description = DESC_INTERNAL_ERROR)
     })
     @GetMapping("/{id}")
     ResponseEntity<PacienteDTO> obtenerPaciente(@PathVariable Long id);
@@ -39,9 +41,9 @@ public interface IPacienteController {
 
     @Operation(summary = "Eliminar paciente por ID", description = "Elimina al paciente con por el campo ID correspondiente", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "204", description = "Paciente eliminado exitosamente"),
-        @ApiResponse(responseCode = "404", description = "No existe el paciente con el ID"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_NO_CONTENT, description = DESC_DELETED),
+        @ApiResponse(responseCode = CODE_NOT_FOUND, description = DESC_NOT_FOUND_ID),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description = DESC_INTERNAL_ERROR)
     })
     @DeleteMapping("/{id}")
     ResponseEntity<Void> eliminarPaciente(@PathVariable Long id);
@@ -49,9 +51,9 @@ public interface IPacienteController {
 
     @Operation(summary = "Buscar paciente por DNI", description = "Busca al paciente con por el campo DNI correspondiente", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Paciente encontrado exitosamente"),
-        @ApiResponse(responseCode = "404", description = "No existe el paciente con el DNI"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_OK, description = DESC_OK),
+        @ApiResponse(responseCode = CODE_NOT_FOUND, description = DESC_NOT_FOUND_DNI),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description = DESC_INTERNAL_ERROR)
     })
     @GetMapping("/buscar/dni")
     ResponseEntity<PacienteDTO> buscarPorDni(@RequestParam String dni);
@@ -59,9 +61,9 @@ public interface IPacienteController {
 
     @Operation(summary = "Buscar paciente por Nombre", description = "Busca al paciente con por el campo Nombre parcial o completo", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Paciente encontrado exitosamente"),
-        @ApiResponse(responseCode = "404", description = "No existe el paciente con el nombre"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_OK, description = DESC_OK),
+        @ApiResponse(responseCode = CODE_NOT_FOUND, description = DESC_NOT_FOUND_NAME),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description = DESC_INTERNAL_ERROR)
     })
     @GetMapping("/buscar/nombre")
     ResponseEntity<List<PacienteDTO>> buscarPorNombre(@RequestParam String nombre);
@@ -69,9 +71,9 @@ public interface IPacienteController {
 
     @Operation(summary = "Actualizar datos del paciente", description = "Actualiza los datos del paciente con los campos ingresados", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Paciente actualizado exitosamente"),
-        @ApiResponse(responseCode = "404", description = "No existe el paciente con el ID"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_OK, description = DESC_UPDATED),
+        @ApiResponse(responseCode = CODE_NOT_FOUND, description = DESC_NOT_FOUND_ID),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description = DESC_INTERNAL_ERROR)
     })
     @PutMapping("/{id}")
     ResponseEntity<PacienteDTO> actualizarPaciente(@PathVariable Long id, @RequestBody PacienteDTO pacienteDTO);
@@ -79,9 +81,9 @@ public interface IPacienteController {
 
     @Operation(summary = "Activar paciente", description = "Activa el paciente con el ID dado", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Paciente activado exitosamente"),
-        @ApiResponse(responseCode = "404", description = "No existe el paciente con el ID"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_OK, description = DESC_ACTIVATED),
+        @ApiResponse(responseCode = CODE_NOT_FOUND, description = DESC_NOT_FOUND_ID),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description = DESC_INTERNAL_ERROR)
     })
     @PatchMapping("/{id}/activar")
     ResponseEntity<PacienteDTO> activarPaciente(@PathVariable Long id);
@@ -89,9 +91,9 @@ public interface IPacienteController {
 
     @Operation(summary = "Desactivar paciente", description = "Desactiva el paciente con el ID dado", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Paciente Desactivado exitosamente"),
-        @ApiResponse(responseCode = "404", description = "No existe el paciente con el ID"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_OK, description = DESC_DEACTIVATED),
+        @ApiResponse(responseCode = CODE_NOT_FOUND, description = DESC_NOT_FOUND_ID),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description = DESC_INTERNAL_ERROR)
     })
     @PatchMapping("/{id}/desactivar")
     ResponseEntity<PacienteDTO> desactivarPaciente(@PathVariable Long id);
@@ -99,8 +101,8 @@ public interface IPacienteController {
 
     @Operation(summary = "Listar pacientes activos", description = "Lista todos los pacientes con estado activo", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Lista de pacientes activos"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_OK, description = DESC_LIST_ACTIVATED),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description =  DESC_INTERNAL_ERROR)
     })
     @GetMapping("/activos")
     ResponseEntity<List<PacienteDTO>> listarPacientesActivos();
@@ -108,8 +110,8 @@ public interface IPacienteController {
 
     @Operation(summary = "Listar pacientes inactivos", description = "Lista todos los pacientes con estado inactivo", tags = {"Paciente"})
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Lista de pacientes inactivos"),
-        @ApiResponse(responseCode = "500", description = "Error del sistema")
+        @ApiResponse(responseCode = CODE_OK, description = DESC_LIST_DEACTIVATED),
+        @ApiResponse(responseCode = CODE_INTERNAL_SERVER_ERROR, description =  DESC_INTERNAL_ERROR)
     })
     @GetMapping("/inactivos")
     ResponseEntity<List<PacienteDTO>> listarPacientesInactivos();
