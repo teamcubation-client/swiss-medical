7d3110cd075087c4f2f154b40c44b952d2d5ae15
fercuche <fer2.maciel@gmail.com>
2025-07-10 13:46:43 -0300

    feat: refactored code to English and added healthPlan and address fields

--- src/main/java/com/tq/pacientes/mappers/PatientMapper.java
+++ src/main/java/com/tq/pacientes/mappers/PatientMapper.java
@@ -1,29 +1,33 @@
 package com.tq.pacientes.mappers;
 
-import com.tq.pacientes.dtos.PacienteDTO;
-import com.tq.pacientes.models.Paciente;
+import com.tq.pacientes.dtos.PatientDTO;
+import com.tq.pacientes.models.Patient;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PacienteMapper {
-    public PacienteDTO toDto(Paciente paciente) {
-        return new PacienteDTO(
-                paciente.getNombre(),
-                paciente.getApellido(),
-                paciente.getDni(),
-                paciente.getObraSocial(), ,
-                ,
-                paciente.getTelefono(), paciente.getEmail());
+public class PatientMapper {
+    public PatientDTO toDto(Patient patient) {
+        return new PatientDTO(
+                patient.getFirstName(),
+                patient.getLastName(),
+                patient.getDni(),
+                patient.getHealthInsurance(),
+                patient.getHealthPlan(),
+                patient.getAddress(),
+                patient.getPhoneNumber(),
+                patient.getEmail());
     }
 
-    public Paciente toEntity(PacienteDTO dto) {
-        Paciente paciente = new Paciente();
-        paciente.setNombre(dto.firstName());
-        paciente.setApellido(dto.lastName());
-        paciente.setDni(dto.dni());
-        paciente.setObraSocial(dto.healthInsurance());
-        paciente.setTelefono(dto.phone());
-        paciente.setEmail(dto.email());
-        return paciente;
+    public Patient toEntity(PatientDTO dto) {
+        Patient patient = new Patient();
+        patient.setFirstName(dto.firstName());
+        patient.setLastName(dto.lastName());
+        patient.setDni(dto.dni());
+        patient.setHealthInsurance(dto.healthInsurance());
+        patient.setHealthPlan(dto.healthPlan());
+        patient.setAddress(dto.address());
+        patient.setPhoneNumber(dto.phoneNumber());
+        patient.setEmail(dto.email());
+        return patient;
     }
 }
--- src/main/java/com/tq/pacientes/models/Patient.java
+++ src/main/java/com/tq/pacientes/models/Patient.java
@@ -1,7 +1,6 @@
 package com.tq.pacientes.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
