84bd6ec9a3a8c442225ac443732dced6bcd33604
Guadalupe Fernandez <guadalupe.fernandez@teamcubation.com>
2025-07-11 13:19:26 -0300

    refactor: add mensaje de error en handlerExcepcionGenerica

--- src/main/java/com/practica/crud_pacientes/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/practica/crud_pacientes/exceptions/GlobalExceptionHandler.java
@@ -5,14 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PacienteDuplicadoException.class)
@@ -40,8 +40,8 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handlerExcepcionGenerica(HttpServletRequest request){
-        String mensaje = "Ocurrió un error inesperado";
+    public ResponseEntity<ErrorResponse> handlerExcepcionGenerica(Exception ex, HttpServletRequest request) {
+        String mensaje = "Ocurrió un error inesperado" + ex.getMessage();
         ErrorResponse error = new ErrorResponse(
                 LocalDateTime.now(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
