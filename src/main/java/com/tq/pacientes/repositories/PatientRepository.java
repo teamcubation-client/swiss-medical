b66953ffddb20b46c7ed41817f05afe45234410d
fercuche <fer2.maciel@gmail.com>
2025-07-17 15:29:43 -0300

    feature: Invoke the Stored Procedures from Java

--- src/main/java/com/tq/pacientes/repositories/PatientRepository.java
+++ src/main/java/com/tq/pacientes/repositories/PatientRepository.java
@@ -2,6 +2,8 @@ package com.tq.pacientes.repositories;
 
 import com.tq.pacientes.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,13 @@ import java.util.Optional;
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
-    Optional<Patient> findByDni(String dni);
+    @Query(value = "CALL find_patient_by_dni(:dni)", nativeQuery = true)
+    Optional<Patient> findByDni(@Param("dni") String dni);
 
-    List<Patient> findByFirstNameContainingIgnoreCase(String firstName);
+    @Query(value = "CALL find_patients_by_first_name(:firstName)", nativeQuery = true)
+    List<Patient> findByFirstNameContainingIgnoreCase(@Param("firstName") String firstName);
+
+    @Query(value = "CALL find_patients_by_health_insurance_paginated(:healthInsurance, :limit, :offset)", nativeQuery = true)
+    List<Patient> findByHealthInsurancePaginated(@Param("healthInsurance") String healthInsurance, int limit, int offset);
+
 }
