c985361d9e3be469bcce05c81bbb7d1b81c3f851
Daniela Gavilan <daniela.gavilan@teamcubation.com>
2025-07-17 09:28:56 -0300

    feat: implementar jpa y uso de store procedures
    - Se agrega nuevo archivo init.sql y store_procedures.sql a carpeta script en raiz del proyecto, reemplazando el uso de archivos .sql en carpeta resource.
    - Nuevos endpoints implementados para el uso de las consultas utilizando sp
    - Se reemplaza el uso de JDBC Template por JPA

--- src/main/java/com/teamcubation/api/pacientes/service/IPacienteService.java
+++ src/main/java/com/teamcubation/api/pacientes/service/IPacienteService.java
@@ -8,6 +8,9 @@ public interface IPacienteService {
     Paciente crear(Paciente request);
     List<Paciente> obtenerTodos(String dni, String nombre);
     Paciente obtenerPorID(long id);
+    Paciente obtenerPorDNI(String dni);
+    List<Paciente> obtenerPorNombre(String nombre);
+    List<Paciente> obtenerPorObraSocial(String obraSocial, int page, int size);
     Paciente actualizarPorID(long id, Paciente paciente);
     void borrarPorID(long id);
 }
