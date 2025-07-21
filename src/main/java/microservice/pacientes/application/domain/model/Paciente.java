ab7f90fe9c2b86dffd89cc4fd5d3fec5c1036876
nicolasberti-tq <nicolas.berti@teamcubation.com>
2025-07-21 15:36:54 -0300

    fix: annotation data for Paciente

--- src/main/java/microservice/pacientes/application/domain/model/Paciente.java
+++ src/main/java/microservice/pacientes/application/domain/model/Paciente.java
@@ -1,12 +1,12 @@
 package microservice.pacientes.application.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @Builder
+@AllArgsConstructor
 public class Paciente {
     private String dni;
     private String nombre;
@@ -14,17 +14,4 @@ public class Paciente {
     private String obraSocial;
     private String email;
     private String telefono;
-    public Paciente(String dni, String nombre, String apellido) {
-        this.dni = dni;
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-    public Paciente(String dni, String nombre, String apellido, String obraSocial, String email, String telefono) {
-        this.dni = dni;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.obraSocial = obraSocial;
-        this.email = email;
-        this.telefono = telefono;
-    }
 }
