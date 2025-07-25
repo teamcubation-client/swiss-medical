b10de2496fff53d07b645c1f142d5fcb5219f5d9
emilianoperez-tq <emiliano.perez@teamcubation.com>
2025-07-25 14:44:24 -0300

    Add TestConstants, Update PatientServiceTest

--- src/test/java/com/swissmedical/patients/application/service/PatientServiceTest.java
+++ src/test/java/com/swissmedical/patients/application/service/PatientServiceTest.java
@@ -1,4 +1,178 @@
 package com.swissmedical.patients.application.service;
 
+import com.swissmedical.patients.application.domain.model.Patient;
+import com.swissmedical.patients.application.domain.ports.out.PatientRepositoryPort;
+import com.swissmedical.patients.shared.exceptions.PatientDuplicateException;
+import com.swissmedical.patients.shared.exceptions.PatientNotFoundException;
+import com.swissmedical.patients.shared.utils.TestContants;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class PatientServiceTest {
+
+  private Patient patient;
+
+  @Mock
+  private PatientRepositoryPort repository;
+
+  @InjectMocks
+  private PatientService patientService;
+
+  @BeforeEach
+  public void setUp() {
+    patient = Patient.builder()
+            .firstName("John")
+            .lastName("Doe")
+            .email(TestContants.EMAIL)
+            .phoneNumber("1234567890")
+            .dni(TestContants.DNI)
+            .memberNumber("MEM12345")
+            .birthDate(LocalDate.of(1990, 1, 1))
+            .isActive(true)
+            .socialSecurity("Swiss Medical")
+            .build();
+  }
+
+  @Test
+  public void testGetAll() {
+    when(repository.findAll(10, 0)).thenReturn(List.of(
+            Patient.builder().firstName("John").lastName("Doe").build(),
+            Patient.builder().firstName("Jane").lastName("Doe").build()
+    ));
+
+    List<Patient> patients = patientService.getAll("", 1, 10);
+
+    assertEquals(2, patients.size());
+  }
+
+  @Test
+  public void testGetAllWithName() {
+    String name = "John";
+    when(repository.findByFirstName(name)).thenReturn(List.of(
+            Patient.builder().firstName("John").lastName("Doe").build()
+    ));
+
+    List<Patient> patients = patientService.getAll(name, 1, 10);
+
+    assertEquals(1, patients.size());
+    assertEquals("John", patients.get(0).getFirstName());
+  }
+
+  @Test
+  public void testGetByDni() {
+    when(repository.findByDni(TestContants.DNI)).thenReturn(Optional.of(patient));
+    Patient result = patientService.getByDni(TestContants.DNI);
+
+    assertEquals(patient, result);
+  }
+
+  @Test
+  public void testGetByDniNotFound() {
+    when(repository.findByDni(TestContants.DNI)).thenReturn(Optional.empty());
+
+    assertThrows(PatientNotFoundException.class, () -> {
+      patientService.getByDni(TestContants.DNI);
+    });
+  }
+
+  @Test
+  public void testGetBySocialSecurity() {
+    String socialSecurity = "swiss-medical";
+
+    when(repository.findBySocialSecurity(socialSecurity, 10, 0)).thenReturn(List.of(
+            Patient.builder().firstName("John").lastName("Doe").socialSecurity("Swiss Medical").build()
+    ));
+
+    List<Patient> patients = patientService.getBySocialSecurity(socialSecurity, 1, 10);
+
+    assertEquals(1, patients.size());
+    assertEquals("John", patients.get(0).getFirstName());
+  }
+
+  @Test
+  public void testGetBySocialSecurityNotFound() {
+    String socialSecurity = "swiss-medical";
+
+    when(repository.findBySocialSecurity(socialSecurity, 10, 0)).thenReturn(List.of());
+
+    assertThrows(PatientNotFoundException.class, () -> {
+      patientService.getBySocialSecurity(socialSecurity, 1, 10);
+    });
+  }
+
+  @Test
+  public void testCreatePatient() {
+    when(repository.existsByDni(TestContants.DNI)).thenReturn(false);
+    when(repository.existsByEmail(TestContants.EMAIL)).thenReturn(false);
+    when(repository.save(any())).thenReturn(patient);
+
+    Patient result = patientService.create(patient);
+
+    // Assertions can be added here to verify the result
+    // For example, you can use assertEquals to check if the returned patient matches the created
+    assertEquals(patient, result);
+  }
+
+  @Test
+  public void shouldThrowErrorWhenDuplicateDni() {
+    when(repository.existsByDni(TestContants.DNI)).thenReturn(true);
+
+    assertThrows(PatientDuplicateException.class, () -> {
+      patientService.create(patient);
+    });
+  }
+
+  @Test
+  public void shouldThrowErrorWhenDuplicateEmail() {
+    String email = "john@gmail.com";
+
+    when(repository.existsByDni(patient.getDni())).thenReturn(false);
+    when(repository.existsByEmail(email)).thenReturn(true);
+
+    assertThrows(PatientDuplicateException.class, () -> {
+      patientService.create(patient);
+    });
+  }
+
+  @Test
+  public void testUpdatePatient() {
+    when(repository.findById(TestContants.ID)).thenReturn(Optional.of(patient));
+    when(repository.save(any())).thenReturn(patient);
+
+    Patient updatedPatient = patientService.update(1L, patient);
+
+    assertEquals(patient, updatedPatient);
+  }
+
+  @Test
+  public void testUpdatePatientNotFound() {
+    when(repository.findById(TestContants.ID)).thenReturn(Optional.empty());
+
+    assertThrows(PatientNotFoundException.class, () -> {
+      patientService.update(1L, patient);
+    });
+  }
+
+  @Test
+  public void testDeletePatientNotFound() {
+    when(repository.existsById(TestContants.ID)).thenReturn(false);
+
+    assertThrows(PatientNotFoundException.class, () -> {
+      patientService.delete(TestContants.ID);
+    });
+  }
 }
---
+++ src/test/java/com/swissmedical/patients/shared/utils/TestContants.java
@@ -0,0 +1,8 @@
+package com.swissmedical.patients.shared.utils;
+
+public class TestContants {
+  public static final String SOCIAL_SECURITY = "Swiss Medical";
+  public static final String DNI = "12345678";
+  public static final String EMAIL = "john@gmail.com";
+  public static final Long ID = 1L;
+}
