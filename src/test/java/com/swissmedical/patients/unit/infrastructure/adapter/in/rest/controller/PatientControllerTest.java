3c2036b8820fc634a334f47377670864fb03916a
emilianoperez-tq <emiliano.perez@teamcubation.com>
2025-07-28 17:45:30 -0300

    Update application.propperties

--- src/test/java/com/swissmedical/patients/unit/infrastructure/adapter/in/rest/controller/PatientControllerTest.java
+++ src/test/java/com/swissmedical/patients/unit/infrastructure/adapter/in/rest/controller/PatientControllerTest.java
@@ -27,8 +27,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PatientController.class)
-@Import(GlobalHandlerException.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class PatientControllerTest {
 
   @Autowired
