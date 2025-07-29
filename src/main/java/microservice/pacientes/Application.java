74b98465ed86b9eeb052ae2720c85caf10505002
Marcos Hwang <marcos.hwang@teamcubation.com>
2025-07-29 17:12:39 -0300

    test unitarios application

--- src/main/java/microservice/pacientes/Application.java
+++ src/main/java/microservice/pacientes/Application.java
@@ -7,8 +7,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(Application.class, args);
     }
 } 
