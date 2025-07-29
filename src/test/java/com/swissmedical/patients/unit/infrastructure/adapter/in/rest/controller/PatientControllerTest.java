1588e51f6dd015fa7081281a838386bef9746389
emilianoperez-tq <emiliano.perez@teamcubation.com>
2025-07-29 10:53:12 -0300

    Update PatientControllerTest

--- src/test/java/com/swissmedical/patients/unit/infrastructure/adapter/in/rest/controller/PatientControllerTest.java
+++ src/test/java/com/swissmedical/patients/unit/infrastructure/adapter/in/rest/controller/PatientControllerTest.java
@@ -11,11 +11,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,8 +26,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ActiveProfiles("test")
-@SpringBootTest
+@WebMvcTest(PatientController.class)
 public class PatientControllerTest {
 
   @Autowired
