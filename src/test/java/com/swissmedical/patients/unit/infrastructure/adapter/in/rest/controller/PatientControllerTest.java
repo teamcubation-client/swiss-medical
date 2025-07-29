0b7b16b876675645888aef0ba7e40cfe45c18165
emilianoperez-tq <emiliano.perez@teamcubation.com>
2025-07-29 10:45:32 -0300

    Update PatientControllerTest

--- src/test/java/com/swissmedical/patients/unit/infrastructure/adapter/in/rest/controller/PatientControllerTest.java
+++ src/test/java/com/swissmedical/patients/unit/infrastructure/adapter/in/rest/controller/PatientControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -26,6 +27,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @WebMvcTest(PatientController.class)
 public class PatientControllerTest {
 
