7a739eb70486db3eaacd755ac53b6d0978b516e6
Guadalupe Fernandez <guadalupe.fernandez@teamcubation.com>
2025-07-30 15:26:34 -0300

    refactor: add constantes para test

---
+++ src/test/java/com/practica/crud_pacientes/utils/TestConstants.java
@@ -0,0 +1,7 @@
+package com.practica.crud_pacientes.utils;
+
+public class TestConstants {
+
+    public static final String ENDPOINT = "/pacientes";
+    public static final int REQUEST_COUNT = 10;
+}
