0feb8e15b6a3cd4ec385034cdb6c86ce68673727
Guadalupe Fernandez <guadalupe.fernandez@teamcubation.com>
2025-07-30 15:23:19 -0300

    refactor: change Mediator por TrafficNotifier

--- src/main/java/com/practica/crud_pacientes/infrastructure/adapter/out/mediator/TrafficMediator.java
+++ src/main/java/com/practica/crud_pacientes/infrastructure/adapter/out/mediator/TrafficMediator.java
@@ -1,17 +1,17 @@
 package com.practica.crud_pacientes.infrastructure.adapter.out.mediator;
 
-import com.practica.crud_pacientes.application.domain.port.in.Mediator;
+import com.practica.crud_pacientes.application.domain.port.in.TrafficNotifier;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
 @Component
-public class TrafficMediator implements Mediator {
+public class TrafficMediator implements TrafficNotifier {
     private final AlertService alertService;
     private final TrafficMonitor trafficMonitor;
 
     @Override
-    public void notifyTraffic(String event) {
+    public void notify(String event) {
         if(trafficMonitor.shouldTriggerAlert()) {
             alertService.sendAlert(event, trafficMonitor.getRequestCount());
             trafficMonitor.reset();
