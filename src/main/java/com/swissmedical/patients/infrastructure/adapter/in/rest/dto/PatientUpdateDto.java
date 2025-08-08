49f9463f45479910f57a614f325e18bdcd28740c
emilianoperez-tq <emiliano.perez@teamcubation.com>
2025-08-08 10:53:50 -0300

    Update PatientCreateDto, PatientUpdateDto

--- src/main/java/com/swissmedical/patients/infrastructure/adapter/in/rest/dto/PatientUpdateDto.java
+++ src/main/java/com/swissmedical/patients/infrastructure/adapter/in/rest/dto/PatientUpdateDto.java
@@ -14,35 +14,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PatientUpdateDto {
-
-  @NotNull(message = "First name cannot be null")
   private String firstName;
-
-  @NotNull(message = "Last name cannot be null")
   private String lastName;
-
-  @NotNull(message = "Email cannot be null")
-  @Email(message = "Email should be valid")
   private String email;
-
-  @NotNull(message = "Phone number cannot be null")
-  //@Size(min = 10, max = 15, message = "Phone number must be between 10 and 15 characters")
   private String phoneNumber;
-
-  @NotNull(message = "DNI cannot be null")
-  @Size(min = 8, max = 10, message = "DNI must be between 8 and 15 characters")
   private String dni;
-
-  //@NotNull(message = "Member number cannot be null")
-  //@Size(min = 5, max = 15, message = "Member number must be between 5 and 15 characters")
   private String memberNumber;
-
-  //@NotNull(message = "Birth date cannot be null")
   private String birthDate;
-
-  //@NotNull(message = "Active status cannot be null")
   private Boolean isActive;
-
-  //@NotNull(message = "Social Security cannot be null")
   private String socialSecurity;
 }
